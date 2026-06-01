289a0131feace8afcb87786845021ad8597668ba
Vaibhav-Ispark <dev.isparkinfo@gmail.com>
2026-06-01 17:25:20 +0530

    fix: improve targeting UI, add all countries, fix redirect URLs for session

--- app/routes/app.settings.targeting.jsx
+++ app/routes/app.settings.targeting.jsx
@@ -5,7 +5,21 @@ import { getAgeGuardConfig, saveAgeGuardConfig } from "../models/ageGuard.server
 import { LivePreview, SettingsShell } from "../components/AgeGuardPreview";
 import { useSettingsForm } from "../components/settingsForm";
 
-const countries = ["US", "CA", "GB", "AU", "DE", "FR", "IN", "NL", "ES", "IT"];
+const countries = [
+  "AF","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BH","BD","BB",
+  "BY","BE","BZ","BJ","BT","BO","BA","BW","BR","BN","BG","BF","BI","CV","KH",
+  "CM","CA","CF","TD","CL","CN","CO","KM","CG","CR","HR","CU","CY","CZ","DK",
+  "DJ","DM","DO","EC","EG","SV","GQ","ER","EE","SZ","ET","FJ","FI","FR","GA",
+  "GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN",
+  "ID","IR","IQ","IE","IL","IT","JM","JP","JO","KZ","KE","KI","KP","KR","KW",
+  "KG","LA","LV","LB","LS","LR","LY","LI","LT","LU","MG","MW","MY","MV","ML",
+  "MT","MH","MR","MU","MX","FM","MD","MC","MN","ME","MA","MZ","MM","NA","NR",
+  "NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PY","PE",
+  "PH","PL","PT","QA","RO","RU","RW","KN","LC","VC","WS","SM","ST","SA","SN",
+  "RS","SC","SL","SG","SK","SI","SB","SO","ZA","SS","ES","LK","SD","SR","SE",
+  "CH","SY","TW","TJ","TZ","TH","TL","TG","TO","TT","TN","TR","TM","TV","UG",
+  "UA","AE","GB","US","UY","UZ","VU","VE","VN","YE","ZM","ZW"
+];
 
 export const loader = async ({ request }) => {
   const { session } = await authenticate.admin(request);
@@ -97,13 +111,19 @@ export default function TargetingSettings() {
                 <option value="exclude">Exclude selected countries</option>
               </select>
             </label>
-            <label className="ag-field">
-              <span className="ag-label">Countries</span>
-              <select multiple value={targeting.countries} onChange={(event) => update("countries", [...event.target.selectedOptions].map((option) => option.value))}>
-                {countries.map((country) => <option key={country}>{country}</option>)}
-              </select>
-            </label>
           </div>
+          <label className="ag-field">
+            <span className="ag-label">Countries (hold Ctrl/Cmd to select multiple)</span>
+            <select
+              multiple
+              size={8}
+              value={targeting.countries}
+              onChange={(event) => update("countries", [...event.target.selectedOptions].map((option) => option.value))}
+              style={{ minHeight: "180px" }}
+            >
+              {countries.map((country) => <option key={country} value={country}>{country}</option>)}
+            </select>
+          </label>
         </section>
       </SettingsShell>
     </div>
